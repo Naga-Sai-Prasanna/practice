3fa122f6fb8602a944ce5fee486fe69101360768
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-04-17 22:05:28 +0530

    test

--- 16-loops.sh
+++ 16-loops.sh
@@ -3,12 +3,13 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell"
 LOGS_FILE="/var/log/shell/$0.log"
 
+mkdir -p $LOGS_FOLDER
+
 if [ $USERID -ne 0 ]; then
  echo "please run the script with root access" | tee -a $LOGS_FILE
  exit 1
 fi
 
-mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
 
@@ -21,11 +22,11 @@ fi
 }
 
 
-for app in $@
+for app in "$@"
 do
-  dnf list installed $app &>>LOGS_FILE
+  dnf list installed $app &>>$LOGS_FILE
   if [ $? -ne 0 ]; then
-   echo "$app is not installed installing now"
+   echo "$app is not installed, installing now"
   else
    echo "$app is installed already"  
    exit 1
