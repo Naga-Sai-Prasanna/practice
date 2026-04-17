688c2b4b261582e64869bd4a009a3c204b3c4e5e
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-04-17 15:51:22 +0530

    test

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,6 +1,11 @@
-#!/bi/bash
-
+#!/bin/bash
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell"
+LOGS_FILE="/var/log/shell/$0.log"
+
+mkdir -p $LOGS_FOLDER
+
+
 if [ $USERID -ne 0 ]; then
  echo "please run the script with root access"
  exit 1
@@ -17,12 +22,12 @@ fi
 }
 
 
-dnf install nginx -y
-VALIDATE $? "installing nginx"
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "installing nginx" 
 dnf remove nginx -y
 VALIDATE $? "removing nginx"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGS_FILE
 VALIDATE $? "installing nodejs"
 dnf remove nodejs -y
 VALIDATE $? "removing nodejs"
