a18f351c9aaa3b4301334786e1dfccdf6460ff19
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-04-17 22:08:45 +0530

    test

---
+++ 18-colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell"
+LOGS_FILE="/var/log/shell/$0.log"
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then
+ echo -e " $R please run the script with root access $N" | tee -a $LOGS_FILE
+ exit 1
+fi
+
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]; then
+   echo -e "$R $2 ... failure $N" | tee -a $LOGS_FILE
+else 
+  echo -e "$G $2 ... success $N" | tee -a $LOGS_FILE
+fi
+
+}
+
+
+for app in "$@"
+do
+  dnf list installed $app &>>$LOGS_FILE
+  if [ $? -ne 0 ]; then
+   echo "$app is not installed, installing now"
+  else
+   echo "$app is installed already"  
+   exit 1
+  fi 
+  dnf install $app -y &>>$LOGS_FILE
+  VALIDATE $? "$app installation" 
+done
