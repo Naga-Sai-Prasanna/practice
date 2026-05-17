0ef069c88f70f8e12987d3f5fa04da493d371234
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-17 20:27:16 +0530

    latest

--- roboshop-infra-dev/10-sg/variables.tf
+++ roboshop-infra-dev/10-sg/variables.tf
@@ -9,3 +9,22 @@ variable "project" {
     default = "roboshop"
   
 }
+
+variable "sg_names" {
+    type = list
+    default = [
+        #databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        #backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        #backend alb
+        "backend_alb",
+        # frontend
+        "frontend",
+        # forntend alb
+        "frontend_alb",
+        #bastion
+        "bastion"
+
+    ]
+}
---
+++ roboshop-infra-dev/20-sg_rules/variables.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+    type = string
+    default = "Dev"
+   
+  
+}
+variable "project" {
+    type = string
+    default = "roboshop"
+  
+}
+
