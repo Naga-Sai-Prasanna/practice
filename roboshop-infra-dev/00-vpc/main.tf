1ed09ffbbe67396ea97d2fb15aa29e6f5f99bdf7
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-18 15:20:09 +0530

    latest

--- roboshop-infra-dev/00-vpc/main.tf
+++ roboshop-infra-dev/00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source =  "git::https://github.com/Naga-Sai-Prasanna/practice.git?ref=main"
+    source = "git::https://github.com/Naga-Sai-Prasanna/practice.git//terraform-aws-vpc?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
--- roboshop-infra-dev/30-bastion/main.tf
+++ roboshop-infra-dev/30-bastion/main.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "bastion" {
     ami  = local.ami_id
     instance_type = "t3.micro"
     subnet_id = local.public_subnet_id
-    vpc_security_group_ids = local.bastion_sg_id
+    vpc_security_group_ids = [local.bastion_sg_id]
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     
     tags = merge(
         {
@@ -12,3 +13,52 @@ resource "aws_instance" "bastion" {
 
     )
 }
+
+
+# iam role 
+
+resource "aws_iam_role" "bastion" {
+  name = "RoboshopDevBastion"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = merge(
+    {
+        Name = "RoboshopDevBastion"
+    },
+    local.common_tags
+  )
+}
+
+#attach a policy
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+
+
+
+# instance profile
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.project}-${var.environment}-bastion"
+  role = aws_iam_role.bastion.name
+}
+
+   
