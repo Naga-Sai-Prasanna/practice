aee8af47126b74fc3e92ac75a9f24c5687a324c4
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-18 19:38:16 +0530

    fixed typo in bootstrap.sh

--- roboshop-infra-dev/40-databases/bootstrap.sh
+++ roboshop-infra-dev/40-databases/bootstrap.sh
@@ -7,5 +7,5 @@ dnf install ansible -y
 cd /home/ec2-user
 git clone https://github.com/Naga-Sai-Prasanna/ansible-roboshop-roles-tf.git
 
-cd ansible-roboshop-roles.tf
+cd ansible-roboshop-roles-tf
 ansible-playbook -e component=$component roboshop.yaml
