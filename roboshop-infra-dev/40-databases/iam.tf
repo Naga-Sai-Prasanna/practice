6ee065824df29b3fa3b382fe8d3324b04a6bc66e
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-19 20:54:36 +0530

    latest

--- roboshop-infra-dev/40-databases/iam.tf
+++ roboshop-infra-dev/40-databases/iam.tf
@@ -25,9 +25,21 @@ resource "aws_iam_role" "mysql" {
   )
 }
 
-# polict creation
+# policy creation
 resource "aws_iam_policy" "mysql" {
   name = local.mysql_policy_name
   description = "A policy for MYSQL Ec2 instance"
   policy = file("mysql-iam-policy.json")
+}
+
+# attaching the policy 
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+#create instance profile
+resource "aws_iam_instance_profile" "mysql" {
+  name = "${var.project}-${var.environment}-mysql"
+  role = aws_iam_role.mysql.name
 }
