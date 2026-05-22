0a954c2f90e577debf2ec3698435d844ec02a634
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-22 16:52:27 +0530

    latest

--- roboshop-infra-dev/60-catalogue/main.tf
+++ roboshop-infra-dev/60-catalogue/main.tf
@@ -47,7 +47,7 @@ provisioner "remote-exec" {
 }
 }
 
-action "aws_ec2_instance_state" "catalogue" {
+resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
   state = "stopped"
   depends_on = [terraform_data.catalogue]
