90d4d478325d80c9884778ef42202f6c4b15bd97
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-25 14:19:30 +0530

    latest

--- roboshop-infra-dev/60-catalogue/locals.tf
+++ roboshop-infra-dev/60-catalogue/locals.tf
@@ -9,5 +9,5 @@ locals {
     }
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-    backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listner_arn.value
+    backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
 }
