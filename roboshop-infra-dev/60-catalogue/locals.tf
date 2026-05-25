6c7bf141fb1e59396bd4bf0551e6643ed5a61398
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-25 13:52:30 +0530

    latest

--- roboshop-infra-dev/60-catalogue/locals.tf
+++ roboshop-infra-dev/60-catalogue/locals.tf
@@ -1,5 +1,6 @@
 locals {
    ami_id = data.aws_ami.joindevops.id
+   vpc_id = data.aws_ssm_parameter.vpc_id.value
   
    common_tags = {
         Project = var.project
@@ -8,5 +9,5 @@ locals {
     }
     catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-
+    backend_alb_listner_arn = data.aws_ssm_parameter.backend_alb_listner_arn.value
 }
--- roboshop-infra-dev/60-catalogue/main.tf
+++ roboshop-infra-dev/60-catalogue/main.tf
@@ -53,6 +53,7 @@ resource "aws_ec2_instance_state" "catalogue" {
   depends_on = [terraform_data.catalogue]
 }
 
+#ami
 resource "aws_ami_from_instance" "catalogue" {
   name = "${var.project}-${var.environment}-catalogue"
   source_instance_id = aws_instance.catalogue.id
@@ -64,4 +65,174 @@ resource "aws_ami_from_instance" "catalogue" {
         local.common_tags
 
     )
+}
+
+# target group
+
+resource "aws_lb_target_group" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+  port = 8080
+  protocol = "HTTP"
+  vpc_id = local.vpc_id
+  deregistration_delay = 60
+
+  health_check {
+    healthy_threshold = 2
+    interval = 10
+    matcher = "200-299"
+    path = "/health"
+    port = 8080
+    protocol = "HTTP"
+    timeout = 2
+    unhealthy_threshold = 3
+  }
+}
+
+# launch template
+
+resource "aws_launch_template" "catalogue" {
+  name = "${var.project}-${var.environment}-catalogue"
+  image_id = aws_ami_from_instance.catalogue.id
+
+  # once autoscaling sees less traffic, it will terminate the instance
+  instance_initiated_shutdown_behavior = "terminate"
+  
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  
+  # each time we apply terraform this version will be updated as default
+  update_default_version = true
+
+  # tags for instances created by launch template through autoscaling
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+        {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+        local.common_tags
+    )
+  
+}
+
+  # tags for volumes created b instances
+  
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+        {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+        local.common_tags
+    )
+  }
+  # tags for launch template
+  tags = merge(
+        {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+        local.common_tags
+  )
+
+
+}
+
+
+# autoscaling
+
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${var.project}-${var.environment}-catalogue"
+  max_size                  = 10
+  min_size                  = 1
+  health_check_grace_period = 120
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = false
+
+  launch_template {
+    id = aws_launch_template.catalogue.id
+    version = "$latest"
+  }
+
+  vpc_zone_identifier       = [aws_subnet.example1.id, aws_subnet.example2.id]
+  target_group_arns = [aws_lb_target_group.catalogue.arn]
+
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["launch_template"]
+  }
+
+  dynamic "tag" {
+    for_each = merge(
+        local.common_tags
+    )
+    content {
+      key = tag.key
+      value = tag.value
+      propagate_at_launch = true 
+    }
+  }
+  # with in 15m autoscalling should be successful
+
+  timeouts {
+    delete = "15m"
+  }
+}
+
+#autoscaling policy
+
+
+resource "aws_autoscaling_policy" "catalogue" {
+  autoscaling_group_name =  aws_autoscaling_group.catalogue.name
+  name = "${var.project}-${var.environment}-catalogue"
+  # track the instances in autoscaling
+  policy_type = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
+
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 70.0
+  }
+} 
+
+#listner rule
+
+resource "aws_lb_listner_rule" "catalogue" {
+  listner_arn = local.backend_alb_arn
+  priority = 10
+
+  action {
+    type = "forward"
+    traget_group_arn = aws_lb_target_group.catalogue.arn
+  }
+  condition {
+    host_header {
+      values = ["catalogue-backend-alb-${var.environment}.${var.domain_name}"]
+    }
+  }
+}
+
+# destroy terraform
+
+resource "terraform_data" "catalogue_delete" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  depends_on = [aws_autoscaling_policy.catalogue]
+
+# it executed in bastion
+  provisioner "local-exec" {
+     command = "aws ec2 terminate-instances  --instance-ids ${aws_instance.catalogue.id}"
+  }
 }
