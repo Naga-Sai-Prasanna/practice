9d9e24c35281981b7c1a35a5472eef40914de385
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-25 13:58:15 +0530

    latest

--- roboshop-infra-dev/50-backend-alb/parameters.tf
+++ roboshop-infra-dev/50-backend-alb/parameters.tf
@@ -4,5 +4,5 @@ resource "aws_ssm_parameter" "sg_id" {
  
   name  = "/${var.project}/${var.environment}/backend_alb_listener_arn"
   type  = "String"
-  value = aws_lb_listner.http.arn
+  value = aws_lb_listener.http.arn
 }
