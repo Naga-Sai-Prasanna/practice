17ec3b71725374476651927ce056430640fc5ff4
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-05-26 21:31:53 +0530

    latest

---
+++ roboshop-infra-dev/80-frontend-alb/variables.tf
@@ -0,0 +1,23 @@
+variable "environment" {
+    type = string
+    default = "Dev"
+   
+  
+}
+variable "project" {
+    type = string
+    default = "roboshop"
+  
+}
+
+
+
+variable "zone_id" {
+  default = "Z08746742X3NL30KWK535"
+
+}
+
+variable "domain_name" {
+  default = "prasanna.fun"
+
+}
