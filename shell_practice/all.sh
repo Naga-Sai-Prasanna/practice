f39deb47af1297734c6057ab9119d0341f6cf53c
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-02 12:19:12 +0530

    service installation

--- shell_practice/all.sh
+++ shell_practice/all.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
-USERID=$(id -u)
-LOG_FOLDER="/var/log/shell-script"
-LOG_FILE="/var/log/shell-script/$0.log"
+# USERID=$(id -u)
+# LOG_FOLDER="/var/log/shell-script"
+# LOG_FILE="/var/log/shell-script/$0.log"
 
 
 
-if [ $USERID -ne 0 ]; then
-  echo "please run the script with root access"
-  exit 1
-fi  
+# if [ $USERID -ne 0 ]; then
+#   echo "please run the script with root access"
+#   exit 1
+# fi  
 
-mkdir -p $LOG_FOLDER
+# mkdir -p $LOG_FOLDER
 
-VALIDATE(){
-   if [ $1 -ne 0 ]; then
-    echo "$2 ....failure" | tee -a $LOG_FILE
-    exit 1
-   else
-    echo "$2...sucees" | tee -a $LOG_FILE
-   fi
-}    
+# VALIDATE(){
+#    if [ $1 -ne 0 ]; then
+#     echo "$2 ....failure" | tee -a $LOG_FILE
+#     exit 1
+#    else
+#     echo "$2...sucees" | tee -a $LOG_FILE
+#    fi
+# }    
 
-dnf install nodejs -y &>> $LOG_FILE
-VALIDATE $? "installing nodejs" 
-
-
+# dnf install nodejs -y &>> $LOG_FILE
+# VALIDATE $? "installing nodejs" 
 
 
 
+
+#loops
+
+for i in (1..20)
+do
+   echo $i
+done
 
 
   
