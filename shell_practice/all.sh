98477d30f0be2012407d63e2cc3dcc562ac32cde
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-02 14:10:54 +0530

    service installation

--- shell_practice/all.sh
+++ shell_practice/all.sh
@@ -37,40 +37,45 @@
 
 
 
-USERID=$(id -u)
-LOG_FOLDER="/var/log/shell-script"
-LOG_FILE="/var/log/shell-script/$0.log"
+# USERID=$(id -u)
+# LOG_FOLDER="/var/log/shell-script"
+# LOG_FILE="/var/log/shell-script/$0.log"
 
 
 
-if [ $USERID -ne 0 ]; then
-  echo "please run the script with root access"
-  exit 1
-fi  
+# if [ $USERID -ne 0 ]; then
+#   echo "please run the script with root access"
+#   exit 1
+# fi  
 
-mkdir -p $LOG_FOLDER
+# mkdir -p $LOG_FOLDER
 
-VALIDATE(){
-   if [ $1 -ne 0 ]; then
-    echo "$2 ....failure" | tee -a $LOG_FILE
-    exit 1
-   else
-    echo "$2...sucees" | tee -a $LOG_FILE
-   fi
-}    
+# VALIDATE(){
+#    if [ $1 -ne 0 ]; then
+#     echo "$2 ....failure" | tee -a $LOG_FILE
+#     exit 1
+#    else
+#     echo "$2...sucees" | tee -a $LOG_FILE
+#    fi
+# }    
 
-for package in $@
-do 
-  dnf list installed $package
-  if [ $? -ne 0 ]; then
-    echo "$package is not installed, installing now"
-    dnf install $package -y
-    VALIDATE $? "$package success"
-  else
-   echo "already installed skipping..now"
-  fi  
-done  
+# for package in $@
+# do 
+#   dnf list installed $package
+#   if [ $? -ne 0 ]; then
+#     echo "$package is not installed, installing now"
+#     dnf install $package -y
+#     VALIDATE $? "$package success"
+#   else
+#    echo "already installed skipping..now"
+#   fi  
+# done  
 
 
 
-  
+  R="\e["31m"
+  G="\e["32m"
+  B="\e["33m"
+  Y="\e["0m"
+
+  echo -e "R hello $N, $Y i am learning linux $N"
