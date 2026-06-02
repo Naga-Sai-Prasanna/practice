c2cf7423478851254febc52d6f3a7322ecadcfc7
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-02 14:30:38 +0530

    service installation

--- shell_practice/all.sh
+++ shell_practice/all.sh
@@ -28,7 +28,7 @@
 # VALIDATE $? "installing nodejs" 
 
 
-#loops
+# loops
 
 # for i in {1..20}
 # do
@@ -36,46 +36,46 @@
 # done
 
 
-
-# USERID=$(id -u)
-# LOG_FOLDER="/var/log/shell-script"
-# LOG_FILE="/var/log/shell-script/$0.log"
-
-
-
-# if [ $USERID -ne 0 ]; then
-#   echo "please run the script with root access"
-#   exit 1
-# fi  
-
-# mkdir -p $LOG_FOLDER
-
-# VALIDATE(){
-#    if [ $1 -ne 0 ]; then
-#     echo "$2 ....failure" | tee -a $LOG_FILE
-#     exit 1
-#    else
-#     echo "$2...sucees" | tee -a $LOG_FILE
-#    fi
-# }    
-
-# for package in $@
-# do 
-#   dnf list installed $package
-#   if [ $? -ne 0 ]; then
-#     echo "$package is not installed, installing now"
-#     dnf install $package -y
-#     VALIDATE $? "$package success"
-#   else
-#    echo "already installed skipping..now"
-#   fi  
-# done  
+set -e
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE="/var/log/shell-script/$0.log"
 
 
 
-  R="\e[31m"
-  G="\e[32m"
-  B="\e[33m"
-  N="\e[0m"
+if [ $USERID -ne 0 ]; then
+  echo "please run the script with root access"
+  exit 1
+fi  
 
-  echo -e "$R hello $N, $B i am learning linux $N"
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+    echo "$2 ....failure" | tee -a $LOG_FILE
+    exit 1
+   else
+    echo "$2...sucees" | tee -a $LOG_FILE
+   fi
+}    
+
+for package in $@
+do 
+  dnf list installed $package
+  if [ $? -ne 0 ]; then
+    echo "$package is not installed, installing now"
+    dnf install $package -y
+    # VALIDATE $? "$package success"
+  else
+   echo "already installed skipping..now"
+  fi  
+done  
+
+
+
+#   R="\e[31m"
+#   G="\e[32m"
+#   B="\e[33m"
+#   N="\e[0m"
+
+#   echo -e "$R hello $N, $B i am learning linux $N"
