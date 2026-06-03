48943a4f109461bc85f549713d114bc8d21a8359
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-03 17:32:09 +0530

    service installation

--- shell_practice/shell-roboshop/user.sh
+++ shell_practice/shell-roboshop/user.sh
@@ -78,23 +78,7 @@ systemctl enable user
 systemctl start user
 VALIDATE $? "enabling and start"
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "COPYING REPO"
 
-dnf install mongodb-mongosh -y
-VALIDATE $? "Client installation"
-
-INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("user")')
-
-    if [ $INDEX -le 0 ]; then
-        mongosh --host $MONGODB_HOST </app/db/master-data.js
-        VALIDATE $? "loading products"
-    else
-       echo -e "products are already loaded.....$Y skipping $N"
-    fi
-
-systemctl restart user
-VALIDATE $? "Restaring user"
 
 
 
