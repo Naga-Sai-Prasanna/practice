214afd30e3f272439dea27a6fc41dc5e4a43a277
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-03 17:06:17 +0530

    service installation

--- shell_practice/shell-roboshop/mysql.sh
+++ shell_practice/shell-roboshop/mysql.sh
@@ -33,5 +33,7 @@ VALIDATE $? "mysql installation"
 systemctl enable mysqld
 VALIDATE $? "enabling the mysql service"
 
-systemctl start mysqld  mysql_secure_installation --set-root-pass RoboShop@1
-VALIDATE $? "set root pass"
+systemctl start mysqld 
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "set root pass
