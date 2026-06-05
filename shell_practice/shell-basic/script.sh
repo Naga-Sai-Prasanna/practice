4659c0df9c9944e68b4f69655fd198428dfc89d2
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-05 10:20:03 +0530

    service installation

---
+++ shell_practice/shell-basic/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd /home/ec2-user
+mkdir app-logs
+cd app-logs
+
+files=(users.log cart.log shipping.log payment.log)
+dates=(2026-05-01 2026-05-02 2026-06-01 2026-06-02)
+
+for i in 0 1 2 3 
+do 
+ touch -d "${dates[$i]}" "${files[$i]}"
+ echo "files are created"
+done
+
