fdaaa3a45d4a113a49e6b85c48945c2cf646a845
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-18 19:13:51 +0530

    terraform

--- roboshop-infra-dev/95-cdn/main.tf
+++ roboshop-infra-dev/95-cdn/main.tf
@@ -93,7 +93,7 @@ resource "aws_route53_record" "cdn" {
 #cdn details
   alias {
     name                   = aws_cloudfront_distribution.roboshop.domain_name
-    zone_id                = aws_cloudfront_distribution.roboshop.domain_name.hosted_zone_id
+    zone_id                = aws_cloudfront_distribution.roboshop.hosted_zone_id
     evaluate_target_health = false
   }
   allow_overwrite = true
