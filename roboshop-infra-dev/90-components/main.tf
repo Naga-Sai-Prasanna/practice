7cc919b6bad182ef97a4f8d66e00cac86d03fbcc
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-18 15:38:58 +0530

    terraform

--- roboshop-infra-dev/90-components/main.tf
+++ roboshop-infra-dev/90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
     source = "git::https://github.com/Naga-Sai-Prasanna/terraform-roboshop-component.git"
-    component_name = each.key
+    component = each.key
     rules_priority = each.value.rule_priority
 }
