70b44a65802b33f7dd19826542fb53248e7a25c4
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-18 11:11:13 +0000

    Fix module source path and remove broken submodule reference

--- roboshop-infra-dev/90-components/main.tf
+++ roboshop-infra-dev/90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "git::https://github.com/Naga-Sai-Prasanna/terraform-roboshop-component.git"
+    source = "git::https://github.com/Naga-Sai-Prasanna/practice.git//terraform-roboshop-component"
     component = each.key
     rule_priority = each.value.rule_priority
 }
