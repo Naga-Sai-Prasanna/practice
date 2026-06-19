d582ce3aed701c5a0759f9d5532839b6530190c8
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-19 12:26:29 +0530

    terraform

--- docker-in/docker.sh
+++ docker-in/docker.sh
@@ -9,7 +9,7 @@ sudo xfs_growfs /var
 
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo  systemctl enable docker
 
