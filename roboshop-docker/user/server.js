0337e66ad099080af99b4f7f5a2b5456c6ddd6d0
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-21 22:43:58 +0530

    actions

--- roboshop-docker/user/server.js
+++ roboshop-docker/user/server.js
@@ -1,7 +1,7 @@
 const instana = require('@instana/collector');
 // init tracing
 // MUST be done before loading anything else!
-// small changes
+// small changes adding
 instana({
     tracing: {
         enabled: true
