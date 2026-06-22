fa69de74909ab7e7782526f6ccce0c35511f1488
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-06-22 10:27:45 +0530

    eksctl

---
+++ eksctl-in/install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# we are creating 50gb root, but only 20gb is partitioned
+# remaining 30gb we need to extend using below commands
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -r -L+30G /dev/RootVG/varVol
+sudo xfs_growfs /var 
+
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo  systemctl enable docker
+
+sudo usermod -aG docker ec2-user
+
+#eksctl
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+
+eksctl version
+
+#kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.35.2/2026-02-27/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+kubectl version --client
---
+++ eksctl-in/main.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "eksctl" {
+  name        = "eksctl-sg"
+  description = "Allow SSH and HTTP on 80 and 8080"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "eksctl-sg"
+    Project   = "roboshop"
+    Component = "dev"
+  }
+}
+resource "aws_instance" "docker" {
+  instance_type = "t3.micro"
+  ami           = "ami-0220d79f3f480ecf5"
+
+  vpc_security_group_ids = [aws_security_group.eksctl.id]
+
+  user_data = file("eksctl.sh")
+
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+
+    tags = {
+      Name = "workstation"
+    }
+  }
+
+  tags = {
+    Name      = "workstation"
+    Project   = "roboshop"
+    Component = "dev"
+  }
+}
