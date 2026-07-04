49264af2d586cc04545543b1f4764c985206e9ee
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-07-04 17:08:47 +0530

    database-subnet

--- terraform-aws-vpc/main.tf
+++ terraform-aws-vpc/main.tf
@@ -209,3 +209,18 @@ resource "aws_route_table_association" "database" {
     route_table_id = aws_route_table.database.id
 
 }
+
+
+# it is for rds (datasubnet)
+
+resource "aws_db_subnet_group" "roboshop" {
+  name       = "${var.project}-${var.environment}"
+  subnet_ids = [aws_subnet.database[0].id,aws_subnet.database[1].id]
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}" # Name the NAT Gateway for better identification
+    }
+  )
+  }
--- terraform-aws-vpc/outputs.tf
+++ terraform-aws-vpc/outputs.tf
@@ -19,3 +19,10 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id  
 }
+
+
+#rds
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.roboshop.name
+}
