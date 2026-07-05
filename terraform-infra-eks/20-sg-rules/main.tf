b748b14aeaf57070bf53f607600702ce9cef0c68
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-07-05 19:05:24 +0530

    actions

--- terraform-infra-eks/20-sg-rules/main.tf
+++ terraform-infra-eks/20-sg-rules/main.tf
@@ -166,3 +166,11 @@ resource "aws_security_group_rule" "eks_node_vpc_cidr" {
   security_group_id  = local.eks_node_sg_id
 }
 
+resource "aws_security_group_rule" "eks_control_plane_eks_node" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1" # all traffic
+  source_security_group_id = local.eks_node_sg_id
+  security_group_id = local.eks_control_plane_sg_id
+}
