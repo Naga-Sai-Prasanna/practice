5e53b83b7dfd0eb1e138cdf9684e59a4922c2131
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-07-12 15:42:05 +0530

    jenkins

--- terraform-infra-eks/31-cicd-tools/main.tf
+++ terraform-infra-eks/31-cicd-tools/main.tf
@@ -53,49 +53,49 @@ resource "aws_instance" "jenkins_agent" {
   )
 }
 
-# resource "aws_instance" "runner" {
-#   count = var.runner ? 1 : 0
-#   ami           = local.ami_id
-#   instance_type = "t3.micro"
-#   subnet_id = local.public_subnet_id
-#   vpc_security_group_ids = [ local.runner_sg_id ]
-#   user_data = file("runner.sh")
+resource "aws_instance" "runner" {
+  count = var.runner ? 1 : 0
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.public_subnet_id
+  vpc_security_group_ids = [ local.runner_sg_id ]
+  user_data = file("runner.sh")
 
-#   root_block_device {
-#     volume_size = 50
-#     volume_type = "gp3"
-#     tags = merge(
-#       {
-#           Name = "${var.project}-${var.environment}-runner"
-#       },
-#     local.common_tags
-#     )
-#   }
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    tags = merge(
+      {
+          Name = "${var.project}-${var.environment}-runner"
+      },
+    local.common_tags
+    )
+  }
 
-#   tags = merge(
-#     {
-#         Name = "${var.project}-${var.environment}-runner"
-#     },
-#     local.common_tags
-#   )
-# }
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-runner"
+    },
+    local.common_tags
+  )
+}
 
-# resource "aws_instance" "sonarqube" {
-#   count = var.sonar ? 1 : 0
-#   ami           = local.sonar_ami_id
-#   instance_type = "t3.large"
-#   vpc_security_group_ids = [local.sonar_sg_id]
-#   subnet_id = local.public_subnet_id #replace your Subnet in default VPC
-#   key_name = "daws-88s"
-#   # need more for terraform
-#   root_block_device {
-#     volume_size = 20
-#     volume_type = "gp3" # or "gp2", depending on your preference
-#   }
-#   tags = merge(
-#     local.common_tags,
-#     {
-#         Name = "${var.project}-${var.environment}-sonar"
-#     }
-#   )
-# }
+resource "aws_instance" "sonarqube" {
+  count = var.sonar ? 1 : 0
+  ami           = local.sonar_ami_id
+  instance_type = "t3.large"
+  vpc_security_group_ids = [local.sonar_sg_id]
+  subnet_id = local.public_subnet_id #replace your Subnet in default VPC
+  key_name = "daws-88s"
+  # need more for terraform
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-sonar"
+    }
+  )
+}
--- terraform-infra-eks/31-cicd-tools/r53.tf
+++ terraform-infra-eks/31-cicd-tools/r53.tf
@@ -18,15 +18,15 @@ resource "aws_route53_record" "jenkins_agent" {
   allow_overwrite = true
 }
 
-# resource "aws_route53_record" "sonarqube" {
-#   count = var.sonar ? 1 : 0
-#   zone_id = var.zone_id
-#   name    = "sonar.${var.domain_name}"
-#   type    = "A"
-#   ttl     = "1"
-#   records = [aws_instance.sonarqube[0].public_ip]
-#   allow_overwrite = true
-# }
+resource "aws_route53_record" "sonarqube" {
+  count = var.sonar ? 1 : 0
+  zone_id = var.zone_id
+  name    = "sonar.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.sonarqube[0].public_ip]
+  allow_overwrite = true
+}
 
 
 ## As Part of CICD ####
@@ -70,22 +70,22 @@ resource "aws_security_group_rule" "jenkins_agent_ssh" {
   security_group_id = local.jenkins_agent_sg_id
 }
 
-# resource "aws_security_group_rule" "sonar_web" {
-#   type              = "ingress"
-#   from_port         = 9000
-#   to_port           = 9000
-#   protocol          = "tcp" # all traffic
-#   # VPC CIDR
-#   cidr_blocks = ["0.0.0.0/0"]
-#   security_group_id = local.sonar_sg_id
-# }
+resource "aws_security_group_rule" "sonar_web" {
+  type              = "ingress"
+  from_port         = 9000
+  to_port           = 9000
+  protocol          = "tcp" # all traffic
+  # VPC CIDR
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = local.sonar_sg_id
+}
 
-# resource "aws_security_group_rule" "sonar_ssh" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp" # all traffic
-#   # VPC CIDR
-#   cidr_blocks = ["0.0.0.0/0"]
-#   security_group_id = local.sonar_sg_id
-# }
+resource "aws_security_group_rule" "sonar_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp" # all traffic
+  # VPC CIDR
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = local.sonar_sg_id
+}
