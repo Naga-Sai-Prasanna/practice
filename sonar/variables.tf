112ee2d98ebb106a460ef0379cdb722d0f551aa1
Naga-Sai-Prasanna <nagasaiprasannak@gmail.com>
2026-07-13 14:19:47 +0530

    jenkins

--- sonar/variables.tf
+++ sonar/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type (needs >=2GB RAM for SonarQube)"
   type        = string
-  default     = "t3.small"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
